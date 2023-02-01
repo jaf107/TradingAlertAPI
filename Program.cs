b06a8ee332223f98e5da6fa9b80fe141ff8bcb54
jaf107 <48786629+jaf107@users.noreply.github.com>
2023-02-01 14:45:13 +0600

    Program.cs Changed

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using TradingAlertAPI.Constants;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+AlertConstant.ClientAPIUrl = builder.Configuration.GetSection("Client_url").Value;
+
 
 var app = builder.Build();
 
