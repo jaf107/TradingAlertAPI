7c41a482c0dca426ad09e332affacdbd874975e3
jaf107 <48786629+jaf107@users.noreply.github.com>
2023-02-05 12:52:32 +0600

    Controller Time Fixed

--- Controllers/AlertController.cs
+++ Controllers/AlertController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text;
+using System.Threading;
 using TradingAlertAPI.Services;
 
 namespace TradingAlertAPI.Controllers
@@ -15,26 +16,31 @@ namespace TradingAlertAPI.Controllers
         {
             StreamWriter? sw = null;
             var req = Request;
-            
+
+            var timeUtc = DateTime.UtcNow;
+            TimeZoneInfo easternZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            DateTime easternTime = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, easternZone);
+
+
             // Read req body and write to file
             StreamReader streamReader = new StreamReader(req.Body, Encoding.UTF8);
             string reqBody = await streamReader.ReadToEndAsync();
             try
             {
                 sw = new StreamWriter("AlertDetails.txt", true);
-                await sw.WriteLineAsync(DateTime.Now + " , " + reqBody);
+                await sw.WriteLineAsync(easternTime + " , " + reqBody);
                 new AlertService().CallExternalAPI(reqBody);
 
             }
             catch (IOException ex)
             {
                 sw = new StreamWriter("AlertErrors.txt", true);
-                await sw.WriteLineAsync(DateTime.Now + " , " + ex.Message);
+                await sw.WriteLineAsync(easternTime + " , " + ex.Message);
             }
             catch (Exception ex)
             {
                 sw = new StreamWriter("AlertErrors.txt", true);
-                await sw.WriteLineAsync(DateTime.Now + " , " + ex.Message);
+                await sw.WriteLineAsync(easternTime + " , " + ex.Message);
             }
             finally
             {
